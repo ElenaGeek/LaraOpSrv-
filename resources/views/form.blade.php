b30d64add17058e579cfe89ad5e678fba51f385e
ElenaGeek <komarele@mail.ru>
2022-01-13 11:23:30 +0300

    'init'

---
+++ resources/views/form.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Lesson 1</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- h1 -->
+<h1>Page1 Form</h1>
+
+<form enctype="multipart/form-data" method="post" action="#">
+    <label for="fname">First name:</label>
+        <input type="text" class="shortext" name="fname" placeholder="Имя"><br>
+    <label for="fname">Last name:</label>
+        <input type="text" class="shortext" name="lname" placeholder="Фамилия"><br>
+    <label for="fname">Your comments:</label>
+        <input type="text" class="longtext" name="comments" placeholder="..."><br>
+ <input type="submit" value="Submit"><br>
+</form>
+
+
+
+</body>
+</html>
